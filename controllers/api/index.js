a71d482b1f4dcfa20bb36697c488aae7d15206a2
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-29 20:28:00 +1100

    added comment routes to view all, create, update and delete... will need further testing once handlebars are active

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 // add routes
 const userRoutes = require('./userRoutes');
 const workspaceRoutes = require('./workspaceRoutes');
+const commentRoutes = require('./commentRoutes');
 
 // initialise routes
 router.use('/users', userRoutes);
 router.use('/workspace', workspaceRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
