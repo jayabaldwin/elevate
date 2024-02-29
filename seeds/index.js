526d08653db2e20068bca9b0623880b5c8e3d5ce
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-29 20:43:29 +1100

    added some seeds and other placeholders

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,16 @@
 const sequelize = require('../config/connection');
-const { Workspace } = require('../models');
+const { Workspace, Comment, User, Project, Task } = require('../models');
 const workspaceData = require('./workspace-seeds.json');
+const commentData = require('./comment-seeds.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await Workspace.bulkCreate(workspaceData, {
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
     returning: true,
   });
 
