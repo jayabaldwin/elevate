05f26c4429b3311449c3c73d648f7ffba7a55ed4
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-29 21:03:30 +1100

    index.js merge confict on api routes fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const userRoutes = require('./userRoutes');
 const workspaceRoutes = require('./workspaceRoutes');
 const commentRoutes = require('./commentRoutes');
 const taskRoutes = require('./taskRoutes');
+const projectRoutes = require('./projectRoutes');
 
 // initialise routes
 router.use('/users', userRoutes);
 router.use('/workspace', workspaceRoutes);
 router.use('/comment', commentRoutes);
 router.use('/task', taskRoutes);
+router.use('/projects', projectRoutes);
 
 module.exports = router;
