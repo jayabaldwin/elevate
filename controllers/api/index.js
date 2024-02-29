82460092feaa967fe13b3918ecc97913d6c7b51f
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-29 20:52:03 +1100

    added task routes into index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const workspaceRoutes = require('./workspaceRoutes');
 const commentRoutes = require('./commentRoutes');
+const taskRoutes = require('./taskRoutes');
 
 // initialise routes
 router.use('/users', userRoutes);
 router.use('/workspace', workspaceRoutes);
 router.use('/comment', commentRoutes);
+router.use('/task', taskRoutes);
 
 module.exports = router;
