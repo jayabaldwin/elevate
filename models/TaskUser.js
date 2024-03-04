5d86554994159f0f781442fce8e2c8f886a4f438
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-03-04 15:26:28 +1100

    corrected import of task

--- models/TaskUser.js
+++ models/TaskUser.js
@@ -1,33 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class TaskUser extends Model { }
+class TaskUser extends Model {}
 
-//Intermediary model for storing the associations between Task and User.
+// Intermediary model for storing the associations between Task and User.
 TaskUser.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        user_id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        task_id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'task_user',
-    }
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    task_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'task_user',
+  }
 );
 
 module.exports = TaskUser;
