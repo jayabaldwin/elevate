176df364407d9aa1c2604211747b1f4692a6edc8
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-03-05 18:15:39 +1100

    comment removal

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,18 +13,6 @@ router.get("/signup", async (req, res) => {
   res.render("signup");
 });
 
-// router.get("/workspace", async (req, res) => {
-//   res.render("newWorkspace");
-// });
-
-// router.get("/create", async (req, res) => {
-//   res.render("newWorkspace");
-// });
-
-// router.get("/join", async (req, res) => {
-//   res.render("newWorkspace");
-// });
-
 // Workspace home /home
 
 module.exports = router;
