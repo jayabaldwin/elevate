50dae6418190a24e9ce28df57496419a690f3e06
Harry P <harryepotter@outlook.com>
2024-03-05 20:14:38 +1100

    Added sortableJS and files to implement

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,6 +5,7 @@ const workspaceRoutes = require('./workspaceRoutes');
 const commentRoutes = require('./commentRoutes');
 const taskRoutes = require('./taskRoutes');
 const projectRoutes = require('./projectRoutes');
+const taskStatusRoutes = require('./taskStatusRoutes');
 
 // initialise routes
 router.use('/users', userRoutes);
@@ -12,5 +13,6 @@ router.use('/workspace', workspaceRoutes);
 router.use('/comment', commentRoutes);
 router.use('/tasks', taskRoutes);
 router.use('/projects', projectRoutes);
+router.use('/', taskStatusRoutes);
 
 module.exports = router;
