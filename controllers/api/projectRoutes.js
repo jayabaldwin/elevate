599b5599cc0e12f46c39e6acc38a016558e26b4a
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-03-09 21:52:38 +1100

    linked modals to buttons, still requires routes and structural build, will do structure tomorrow + added to do comments on some pages

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,24 +1,26 @@
-const router = require('express').Router();
-const { Project, Workspace, User, Task } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Project, Workspace, User, Task } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-// Opening to see projects, will display all projects
-router.get('/', withAuth, async (req, res) => {
+// CONNECT TO PROJECT JS AND LINK UP ROUTES TO RENDER ON DASHBOARD AND SIDEBAR
+// Jaya will add styling later
+// Dashboard and sidebar to display all projects
+// Connect post request to modal that is displayed in 2 locations (navbar and dashboard card)
+router.get("/", withAuth, async (req, res) => {
   try {
-
     const workspaceData = await Workspace.findAll({
-      include: [{ model: Project, include: Task }]
-    })
+      include: [{ model: Project, include: Task }],
+    });
 
     const workspaces = [];
 
-    workspaceData.forEach(workspace => {
+    workspaceData.forEach((workspace) => {
       workspacePlain = workspace.get({ plain: true });
       workspaces.push(workspacePlain);
     });
 
-    res.render('projects', {
-      workspaces
+    res.render("dashboard", {
+      workspaces,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -26,16 +28,16 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 // Selecting a project will display individual project by id
-router.get('/:id', withAuth, async (req, res) => {
+router.get("/:id", withAuth, async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id, {
-      include: [{ model: Workspace }, { model: User, attributes: ['name'] }],
+      include: [{ model: Workspace }, { model: User, attributes: ["name"] }],
     });
 
     if (!projectData) {
       res
         .status(404)
-        .json({ message: 'Error finding Project matching this id' });
+        .json({ message: "Error finding Project matching this id" });
       return;
     }
     res.status(200).json(projectData);
@@ -45,7 +47,7 @@ router.get('/:id', withAuth, async (req, res) => {
 });
 
 // Creating new project
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newProject = await Project.create({
       ...req.body,
