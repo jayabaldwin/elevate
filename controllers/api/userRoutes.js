5b70d4dfbd4f657ddb809b35e76ba2695d205621
adzy404 <adam.isitmez@hotmail.com>
2024-03-09 19:40:54 +1100

    added a save of workspace id to session

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -8,6 +8,7 @@ router.post("/", async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      req.session.workspace_id = userData.workspace_id;
 
       res.status(200).json(userData);
     });
@@ -42,6 +43,7 @@ router.post("/login", async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      req.session.workspace_id = userData.workspace_id;
 
       res.json({ user: userData, message: "You are now logged in!" });
     });
